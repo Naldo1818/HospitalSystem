f2ac05b326eb16f665ca3bfb5eff7ad6fdb9431f
Zaid Lukin Smiles <s223162108@mandela.ac.za>
2024-06-17 22:48:58 +0200

    Nurse Basic pages

--- Controllers/NurseController.cs
+++ Controllers/NurseController.cs
@@ -14,7 +14,46 @@ namespace DEMO.Controllers
             return View();
 
         }
-
-
+        public IActionResult AdmissionPageVital()
+        {
+            return View();
+        }
+        public IActionResult Discharge()
+        {
+            return View();
+        }
+        public IActionResult DischargeList()
+        {
+            return View();
+        }
+        public IActionResult ViewSurgeryBooking()
+        {
+            return View();
+        }
+        public IActionResult AdmissionDetailsWard()
+        {
+            return View();
+        }
+        public IActionResult AdmissionConditionsAllergy()
+        {
+            return View();
+        }
+        public IActionResult AdmittedPatients()
+        {
+            return View();
+        }
+        public IActionResult MedicationCollection()
+        {
+            return View();
+        }
+        public IActionResult MedicationAdministration()
+        {
+            return View();
+        }
+        public IActionResult PatientRecords()
+        {
+            return View();
+        }
+        
     }
 }
