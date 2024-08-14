04d1a7e518ae0739dd61617873e5f68dfd223518
Zaid Lukin Smiles <s223162108@mandela.ac.za>
2024-08-14 22:13:19 +0200

    Admission Model

--- Models/NurseModels/AdmissionModel.cs
+++ Models/NurseModels/AdmissionModel.cs
@@ -1,6 +1,29 @@
-﻿namespace DEMO.Models.NurseModels
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DEMO.Models.NurseModels
 {
     public class AdmissionModel
     {
+        [Key]
+        public int AdmissionId { get; set; }
+
+        [Required]
+        [ForeignKey("BookingID")]
+        public int BookingID { get; set; }
+        
+        [Required]
+        [ForeignKey("PatientID")]
+        public int PatientID {  get; set; }
+
+        [Required]
+        [ForeignKey("WardID")]
+        public int WardID { get; set;}
+
+        [Required]
+        public DateTime Date = DateTime.Now;
+
+        [Required]
+        public string AdmissionStatus { get; set; } 
     }
 }
