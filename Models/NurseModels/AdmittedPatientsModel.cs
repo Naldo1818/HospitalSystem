3111362cb24779b5ddb4cffd6e7891ebb148e5fa
Zaid Lukin Smiles <s223162108@mandela.ac.za>
2024-08-22 11:46:27 +0200

    nurse changes

--- Models/NurseModels/AdmittedPatientsModel.cs
+++ Models/NurseModels/AdmittedPatientsModel.cs
@@ -1,6 +1,32 @@
-﻿namespace DEMO.Models.NurseModels
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DEMO.Models.NurseModels
 {
     public class AdmittedPatientsModel
     {
+        [Key]
+        public int AdmissionId { get; set; }
+
+        [Required]
+        [ForeignKey("BookingID")]
+        public int BookingID { get; set; }
+
+        [Required]
+        [ForeignKey("PatientID")]
+        public int PatientID { get; set; }
+
+        [Required]
+        [ForeignKey("WardID")]
+        public int WardID { get; set; }
+
+        [Required]
+        public DateTime Date = DateTime.Now;
+
+        [Required]
+        public string AdmissionStatus { get; set; }
+
+        public BookSurgery bookSurgery { get; set; }
+        public List<BookingModel> bookings { get; set; }
     }
 }
--- Models/NurseModels/BookingModel.cs
+++ Models/NurseModels/BookingModel.cs
@@ -1,6 +1,25 @@
-﻿namespace DEMO.Models.NurseModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DEMO.Models.NurseModels
 {
     public class BookingModel
     {
+        [Key]
+        public int BookingID { get; set; }
+        [Required]
+        public int AccountID { get; set; }
+        [Required]
+        public int PatientID { get; set; }
+
+        [Required]
+        public string SurgeryTime { get; set; }
+
+        [Required]
+        public DateOnly SurgeryDate { get; set; }
+
+        [Required]
+        public string Theater { get; set; }
+
+        
     }
 }
