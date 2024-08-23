1888db16250d46d2babe2596e64aaa877b879939
Zaid Lukin Smiles <s223162108@mandela.ac.za>
2024-08-23 18:00:10 +0200

    email

---
+++ Models/NurseModels/EmailVital.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DEMO.Models.NurseModels
+{
+    public class EmailVital
+    {
+        public int AccountID { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Notes { get; set; }
+
+        [Required]
+        public VitalsModel Vitals { get; set; }
+
+    }
+}
