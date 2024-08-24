5d80a90e426596bf3a2bd4a3fb2ac29c9906b03a
Zaid Lukin Smiles <s223162108@mandela.ac.za>
2024-08-24 16:33:16 +0200

    add mediccation changes

---
+++ Data/Migrations/20240824142341_PharmacyMedicationTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DEMO.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PharmacyMedicationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PharmacyMedication",
+                columns: table => new
+                {
+                    PharmacyMedicationlID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DosageForm = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Schedule = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StockonHand = table.Column<int>(type: "int", nullable: false),
+                    ReorderLevel = table.Column<int>(type: "int", nullable: false),
+                    ActiveIngredientsAndStrength = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PharmacyMedication", x => x.PharmacyMedicationlID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PharmacyMedication");
+        }
+    }
+}
