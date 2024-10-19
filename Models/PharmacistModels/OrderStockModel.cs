0272b24185fe8fca610e8ebb532e6c277440e116
Samuel Mensah <s223130680@M104-23>
2024-10-19 05:09:48 +0200

    .

--- Models/PharmacistModels/OrderStockModel.cs
+++ Models/PharmacistModels/OrderStockModel.cs
@@ -9,10 +9,20 @@ namespace DEMO.Models.PharmacistModels
         [Key]
         public int StockOrderID { get; set; }
 
-  
+        public string medName {  get; set; }
+
+        public string dosageform {  get; set; }
+
+        public int stockonhand {  get; set; }
+
+        public int amounttoorder {  get; set; }
         
 
         public int PharmacyMedicationlID { get; set; }
+
+
+
+
 
 
         [Required]
